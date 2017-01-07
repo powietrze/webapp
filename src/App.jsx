3e230073eb8f5ac82765f06b86b990bd22a57b66
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2017-01-07 19:10:18 +0100

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
+import {Link} from 'inferno-router';
 
 
 export class App extends Component {
@@ -7,6 +8,7 @@ export class App extends Component {
     return (
       <div>
         Hello Powietrze from Inferno!
+        {this.props.children}
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,39 @@
 
 
 import Inferno from 'inferno';
+import {Router, Route, IndexRoute, Link} from 'inferno-router';
+import {createBrowserHistory} from 'history';
 
-import { App } from './App.jsx';
+import {App} from './App.jsx';
 
 
-Inferno.render(<App />, document.getElementById('root'));
+const Main = () => (
+  <div>
+    Main
+    <Link to="/a">Go to A</Link>
+  </div>
+);
+
+const A = () => (
+  <div>
+    Route A. Go to: <Link to="/">Root</Link> <Link to="/b">B</Link>
+  </div>
+);
+
+const B = () => (
+  <div>
+    Route B. Go to: <Link to="/">Root</Link> <Link to="/a">A</Link>
+  </div>
+);
+
+const routes = (
+  <Router history={ createBrowserHistory() }>
+    <Route component={App}>
+      <IndexRoute component={Main}/>
+      <Route path="a" component={A}/>
+      <Route path="b" component={B}/>
+    </Route>
+  </Router>
+);
+
+Inferno.render(routes, document.getElementById('root'));
