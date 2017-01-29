38d1d9abe9b2b23e50e7f8566bfccb1df0a3c5e2
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2017-01-29 18:33:59 +0100

    Styles station list.

--- src/components/StationList.jsx
+++ src/components/StationList.jsx
@@ -4,6 +4,8 @@ import React, {
 } from 'react';
 
 import { Station } from './Station';
+import styles from './StationList.css';
+
 
 export class StationList extends PureComponent {
   static propTypes = {
@@ -18,8 +20,9 @@ export class StationList extends PureComponent {
     const { isLoading, stations } = this.props;
 
     return (
-      <div>
+      <div className={styles.StationList}>
         {isLoading && 'Ładuję...'}
+        {!isLoading && <span className={styles.Title}>Stacje:</span>}
         {!isLoading && stations.map(s => <Station key={s.id} id={s.id}>{s.name}</Station>)}
       </div>
     );
