ce0d1329fec57c1553cebcc872e9db1971a7ed40
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2017-01-29 19:05:50 +0100

    Added production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: [
+    './src/index.jsx',
+  ],
+  output: {
+    filename: 'bundle-[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new webpack.DefinePlugin({
+      GOOGLE_MAPS_API_KEY: JSON.stringify(process.env.GOOGLE_MAPS_API_KEY),
+      API_HOST: JSON.stringify(process.env.API_HOST),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.css?$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]-[local]__[hash:base64:5]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+};
